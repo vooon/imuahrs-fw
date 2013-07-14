556a7ae2c1f7bf7394cd5a8b879e7c9cfbffb7a0
Vladimir Ermakov <vooon341@gmail.com>
2013-07-14 23:18:22 +0400

    Add program target using openocd + stlink-v2

---
+++ config/openocd.mk
@@ -0,0 +1,15 @@
+# -*- Makefile -*-
+
+OPENOCD=openocd
+OPENOCD_CONF=$(AHRS)/config/openocd_imuahrs.cfg
+OPENOCD_FLASH_BASE=0x08000000
+
+program: $(AHRS)/build/$(PROJECT).bin
+	$(OPENOCD) -f $(OPENOCD_CONF) \
+		-c init \
+		-c "reset halt" \
+		-c "flash write_image erase $< $(OPENOCD_FLASH_BASE) bin" \
+		-c "verify_image $< $(OPENOCD_FLASH_BASE) bin" \
+		-c "reset run" \
+		-c "shutdown"
+
